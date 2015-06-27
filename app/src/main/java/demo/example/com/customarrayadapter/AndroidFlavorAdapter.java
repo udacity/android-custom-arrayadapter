b56775283748f7d37c6e75c4e6a5b320eef4fdd6
Daniel Mai <daniel@udacity.com>
2015-06-27 11:24:15 -0700

    docs: Add comments to custom array adapter.

--- app/src/main/java/demo/example/com/customarrayadapter/AndroidFlavorAdapter.java
+++ app/src/main/java/demo/example/com/customarrayadapter/AndroidFlavorAdapter.java
@@ -13,13 +13,34 @@ import java.util.List;
 public class AndroidFlavorAdapter extends ArrayAdapter<AndroidFlavor> {
     private static final String LOG_TAG = AndroidFlavorAdapter.class.getSimpleName();
 
-    public AndroidFlavorAdapter(Activity context, List<AndroidFlavor> androidFlavors)
-    {
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the List is the data we want
+     * to populate into the lists
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param androidFlavors A List of AndroidFlavor objects to display in a list
+     */
+    public AndroidFlavorAdapter(Activity context, List<AndroidFlavor> androidFlavors) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, androidFlavors);
     }
 
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The AdapterView position that is requesting a view
+     * @param convertView The recycled view to populate.
+     *                    (search online for "android view recycling" to learn more)
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Gets the AndroidFlavor object from the ArrayAdapter at the appropriate position
         AndroidFlavor androidFlavor = getItem(position);
         View rootView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_flavor, parent, false);
 
